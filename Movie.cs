530549c5264fe52f4298518dd7c0e3e1ed401262
Veriko Nikuradze <veriko.nikuradze@caucasusauto.com>
2025-10-07 16:01:21 +0400

    first commit

---
+++ Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieConsoleApp
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+
+
+        public Movie(string title, string director, int releaseYear)
+        {
+            Title = title;
+            Director = director;
+            ReleaseYear = releaseYear;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Title} ({ReleaseYear}), directed by {Director}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MovieConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("The Matrix", "Lana Wachowski", 1999);
+            Movie movie2 = new Movie("The Fifth Element", "Luc Besson", 1997);
+            Movie movie3 = new Movie("Blade Runner", "Ridley Scott", 1982);            
+
+            movie1.PrintInfo();
+            movie2.PrintInfo();
+            movie3.PrintInfo();
+
+
+            /*List<Movie> movies = new List<Movie>()
+            {
+                new Movie("The Matrix", "Lana Wachowski", 1999),
+                new Movie("The Fifth Element", "Luc Besson", 1997)
+            };
+
+            foreach (var movie in movies)
+            {
+                movie.PrintInfo();
+            }*/
+
+        }
+    }
+}
