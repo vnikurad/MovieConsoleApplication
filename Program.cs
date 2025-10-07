b30638a24ded8b5537d5df137d0d22c206af679b
vnikurad <veriko.nikuradze@gmail.com>
2025-10-07 16:22:56 +0400

    simplify "new" statement

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Movie movie1 = new Movie("The Matrix", "Lana Wachowski", 1999);
-            Movie movie2 = new Movie("The Fifth Element", "Luc Besson", 1997);
-            Movie movie3 = new Movie("Blade Runner", "Ridley Scott", 1982);            
+            Movie movie1 = new("The Matrix", "Lana Wachowski", 1999);
+            Movie movie2 = new("The Fifth Element", "Luc Besson", 1997);
+            Movie movie3 = new("Blade Runner", "Ridley Scott", 1982);            
 
             movie1.PrintInfo();
             movie2.PrintInfo();
