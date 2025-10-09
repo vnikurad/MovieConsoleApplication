5c345ff12bdda6ed08435439dd50a41369812948
Veronique <veriko.nikuradze@gmail.com>
2025-10-09 17:25:37 +0400

    add interface

--- Movie.cs
+++ Movie.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MovieConsoleApp
 {
-    public class Movie
+    public class Movie : IMovie
     {
         public string Title { get; set; }
         public string Director { get; set; }
--- Program.cs
+++ Program.cs
@@ -4,25 +4,28 @@
     {
         static void Main(string[] args)
         {
+            /*
             Movie movie1 = new("The Matrix", "Lana Wachowski", 1999);
             Movie movie2 = new("The Fifth Element", "Luc Besson", 1997);
-            Movie movie3 = new("Blade Runner", "Ridley Scott", 1982);            
+            Movie movie3 = new("Blade Runner", "Ridley Scott", 1982);
 
             movie1.PrintInfo();
             movie2.PrintInfo();
-            movie3.PrintInfo();
+            movie3.PrintInfo(); 
+            */
 
 
-            /*List<Movie> movies = new List<Movie>()
+            List<Movie> movies = new List<Movie>()
             {
                 new Movie("The Matrix", "Lana Wachowski", 1999),
-                new Movie("The Fifth Element", "Luc Besson", 1997)
-            };
+                new Movie("The Fifth Element", "Luc Besson", 1997),
+                new Movie("Blade Runner", "Ridley Scott", 1982)
+        };
 
             foreach (var movie in movies)
             {
                 movie.PrintInfo();
-            }*/
+            }
 
         }
     }
